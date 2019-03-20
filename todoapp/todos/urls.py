2f3830817412edcd2eb99fbe54038894609d3134
mohit12r <32235347+mohit12r@users.noreply.github.com>
2019-03-20 11:11:30 +0530

    Add files via upload

---
+++ todoapp/todos/urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 22:25:17 2019
+
+@author: abc
+"""
+
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns=[
+        url(r'^$',views.index,name='index'),
+        url(r'^details/(?P<id>\w{0,50})/$', views.details),
+        url(r'^add',views.add,name='add'),
+       
+        ]
